0bc6c50fff3a9277d63a1bb3586157d63ae6d17a
Andrey <84859114+Andrey1039@users.noreply.github.com>
2023-01-09 18:20:23 +0500

    Обновлена программа

---
+++ Weather/Weather/MainWindow.xaml.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Net.Http;
+using Microsoft.Win32;
+using System.Globalization;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        string url;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.url = GetUrl();
+            GetWeather(url);
+
+            timer = new DispatcherTimer();
+            UpdateTimer();
+
+            SystemEvents.SessionEnded += this.SystemEvents_Power;
+            NetworkChange.NetworkAvailabilityChanged += this.NetworkChange_NetworkAvailabilityChanged;
+        }
+
+        private void SystemEvents_Power(object sender, SessionEndedEventArgs e)
+        {
+            Dispatcher.Invoke(() => GetWeather(url));
+        }
+
+        private void NetworkChange_NetworkAvailabilityChanged(object? sender, NetworkAvailabilityEventArgs e)
+        {
+            if (e.IsAvailable)
+            {
+                Dispatcher.Invoke(() => GetWeather(url));
+            }
+        }
+
+        // Загрузка ссылки на город из файла
+        private string GetUrl()
+        {
+            string url = "https://weather.com/ru-RU/weather/today/l/464c57daded904c43a47deedb004aa696e9e6cc12f5907e7f4c884ff8cfd9157";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\Data.txt");
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string? line = string.Empty;
+                    if ((line = reader.ReadLine()) != null)
+                        url = line;
+                }
+            }
+            catch
+            {
+                return url;
+            }
+
+            return url;
+        }
+
+        // Сохранение ссылки на город в файл
+        private void SaveUrl()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\Data.txt");
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    writer.WriteLine(url);
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        // Обновление погоды в заданном интервале
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            GetWeather(url);
+        }
+
+        // Установка таймера
+        private void UpdateTimer()
+        {
+            int updateTime = 20;
+
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = new TimeSpan(0, updateTime, 0);
+            timer.Start();
+        }
+
+        // Получение данных со страницы погоды
+        private async void GetWeather(string url)
+        {
+            ErrorInfo.Visibility = Visibility.Hidden;
+            LoadInfo.Visibility = Visibility.Visible;
+
+            await Task.Run(() =>
+            {
+                string doc = GetHtml(url);
+                WeatherData data = new WeatherData(doc);
+                Dispatcher.Invoke(() => ShowWeather(data));
+            });
+        }
+
+        // Сопоставление длины текста и размеров поля вывода Label
+        private bool IsTextTrimmed(string text, System.Windows.Controls.Label label)
+        {
+            var typeface = new Typeface(label.FontFamily, label.FontStyle, label.FontWeight, label.FontStretch);
+
+            FormattedText formattedText = new FormattedText(text, CultureInfo.CurrentCulture,
+            label.FlowDirection, typeface, label.FontSize, label.Foreground,
+            VisualTreeHelper.GetDpi(label).PixelsPerDip);
+
+            return formattedText.Width > label.Width;
+        }
+
+        // Вывод данных о погоде на экран
+        private void ShowWeather(WeatherData data)
+        {
+            try
+            {
+                WeatherInfo.Visibility = Visibility.Visible;
+                WeatherInfo.Background = new ImageBrush(new BitmapImage(new Uri(data.ImageSourse)));
+                LoadInfo.Visibility = Visibility.Hidden;
+            }
+            catch
+            {
+                WeatherInfo.Visibility = Visibility.Hidden;
+                LoadInfo.Visibility = Visibility.Hidden;
+                ErrorInfo.Visibility = Visibility.Visible;
+                return;
+            }
+
+            TempLabel.Content = data.Temperature;
+            TempLabel.Foreground = new SolidColorBrush(data.FontColor);
+
+            StatusTB.Text = data.WeatherStatus;            
+            StatusLabel.ToolTip = IsTextTrimmed(data.WeatherStatus, StatusLabel) ? data.WeatherStatus : null;
+            StatusLabel.Foreground = new SolidColorBrush(data.FontColor);           
+
+            CityTB.Text = data.City;
+            CityLabel.ToolTip = IsTextTrimmed(data.City, CityLabel) ? data.City : null;
+            CityLabel.Foreground = new SolidColorBrush(data.FontColor);
+        }
+
+        // Получение страницы с данными о погоде
+        private string GetHtml(string url)
+        {
+            string pageContent = string.Empty;            
+            
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36");
+            try
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        pageContent = content.ReadAsStringAsync().Result;
+                    }
+                }
+            }
+            catch
+            {
+                return pageContent;
+            }
+
+            return pageContent;
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+            MainGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void MoveWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ProgramWindow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Buttons.Visibility = Visibility.Visible;
+        }
+
+        private void ProgramWindow_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Buttons.Visibility = Visibility.Hidden;
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsInfo.Visibility = Visibility.Visible;
+        }
+
+        private void OkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsInfo.Visibility = Visibility.Hidden;
+            WeatherInfo.Visibility = Visibility.Hidden;
+
+            url = CityLinkTB.Text;
+            CityLinkTB.Text = string.Empty;
+            SaveUrl();
+            GetWeather(url);
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CityLinkTB.Text = string.Empty;
+            SettingsInfo.Visibility = Visibility.Hidden;
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {            
+            GetWeather(url);
+        }
+    }
+}
---
+++ Weather/Weather/WeatherData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+
+namespace Weather
+{
+    class WeatherData
+    {
+        public string Temperature { get; }
+        public string WeatherStatus { get; }
+        public string City { get; }
+        public string ImageSourse { get; }
+        public Color FontColor { get; }
+
+        public WeatherData(string html)
+        {
+            this.Temperature = GetTemperature(html);
+            this.WeatherStatus = GetWeatherStatus(html);
+            this.City = GetCity(html);
+            this.ImageSourse = GetImage(html);
+            this.FontColor = GetColorFont(html);
+        }
+
+        // Определение цвета шрифта
+        private Color ColorFont(DateTime sunrise, DateTime sunset) 
+        {
+            if (DateTime.Now >= sunrise && DateTime.Now <= sunset)
+                return Colors.Black;
+            else return Colors.White;
+        }
+
+        // Получение текущей температуры
+        private string GetTemperature(string html)
+        {
+            string temperature = Regex.Match(html, 
+                @"<span data-testid=""TemperatureValue"" class=""CurrentConditions--tempValue--.*?"">(\W*\d+\W)</span>").Groups[1].Value;
+
+            return temperature;
+        }
+
+        // Получение текущего погодного статуса
+        private string GetWeatherStatus(string html)
+        {
+            string weatherStatus = Regex.Match(html, 
+                @"<div data-testid=""wxPhrase"" class=""CurrentConditions--phraseValue--.*?"">(.*?)</div>").Groups[1].Value;
+
+            return weatherStatus;
+        }
+
+        // Получение региона
+        private string GetCity(string html)
+        {
+            string city = Regex.Match(html,
+                @"<h1 class=""CurrentConditions--location--.*?>(.*?)</h1>").Groups[1].Value;
+
+            return city;
+        }
+
+        // Получение изображения погоды
+        private string GetImage(string html)
+        {
+            string imageSourse = Regex.Match(html, @"style=""background-image:url\((.*?)\)").Groups[1].Value;
+
+            return imageSourse;
+        }
+
+        // Получение времени заката и рассвета
+        private Color GetColorFont(string html)
+        {
+            Match time = Regex.Match(html, 
+                @"<p class=""SunriseSunset--dateValue--.*?"">(\d+[:]\d+)</p></div>");
+
+            DateTime sunrise, sunset;
+            DateTime.TryParse(time.Groups[1].Value, out sunrise);
+
+            time = time.NextMatch();
+            DateTime.TryParse(time.Groups[1].Value, out sunset);
+
+            Color colorFont = ColorFont(sunrise, sunset);
+
+            return colorFont;
+        }
+    }
+}
